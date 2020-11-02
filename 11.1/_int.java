6541e4fd9fe25796e3653a307fc64ad6c906b6a8
czy150 <72090318+czy150@users.noreply.github.com>
2020-11-03 00:58:09 +0800

    11.2

---
+++ 11.1/_int.java
@@ -0,0 +1,15 @@
+public class _int {
+  // 实例变量name
+  public String name;
+  // 实例变量age
+  public int age;
+  
+  public static void main(String[] args) {
+    int a = 20;
+    System.out.println("修改前变量a=" + a);
+    a = 100;
+    System.out.println("修改后变量a=" + a);
+    a = 234;
+  }
+  
+}
---
+++ 11.2/ArithmeticOperators1.java
@@ -0,0 +1,10 @@
+public class ArithmeticOperators1 {
+  public static void main(String[] args) {
+      // 声明两个整型变量 num1,num2 分别赋值为 2, 3
+      int num1 = 2;
+      float num2 = 3.45;
+      // 使用加法运算符，对num1和num2执行相加操作，并将返回结果赋值给result
+      float result = num1 + num2;
+      System.out.println( num1 + " + " + num2 + " = " + result);
+  }
+}
